e9b9a2e5c63e0119a8a35041e4fce77981ecccea
77dlbk <amangazytoktombaev@gmail.com>
2025-02-17 22:50:55 +0600

    hw5

---
+++ app/src/main/java/com/example/noteapp/UI/Fragments/Note/NoteFragment.kt
@@ -0,0 +1,27 @@
+package com.example.noteapp.UI.Fragments.Note
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.noteapp.R
+import com.example.noteapp.databinding.FragmentNoteBinding
+
+class NoteFragment : Fragment() {
+
+    private lateinit var binding: FragmentNoteBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNoteBinding.inflate(layoutInflater , container , false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
