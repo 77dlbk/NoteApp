2f483225ed6d54467dfc61d24a6994e2ca7cae0a
77dlbk <amangazytoktombaev@gmail.com>
2025-03-05 13:01:13 +0600

    hw8

--- app/src/main/java/com/example/noteapp/UI/Activity/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/UI/Activity/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.noteapp.UI.Activity
 
+import android.app.ProgressDialog.show
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.noteapp.R
 import com.example.noteapp.databinding.ActivityMainBinding
 import com.example.noteapp.utils.PreferenceHelper
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
@@ -21,6 +28,27 @@ class MainActivity : AppCompatActivity() {
         PreferenceHelper.init(this)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val drawerLayout: DrawerLayout =  findViewById(R.id.drawer_layout)
+        val navView:NavigationView = findViewById(R.id.nav_view)
+
+        toggle=ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R. id.nav_onboard -> Toast.makeText(applicationContext, "Clicked On Board",Toast .LENGTH_SHORT).show()
+                R. id.nav_notes-> Toast.makeText(applicationContext, "Clicked Notes",Toast .LENGTH_SHORT).show()
+                R. id.nav_onboard -> Toast.makeText(applicationContext, "Clicked Auth",Toast .LENGTH_SHORT).show()
+
+
+
+            }
+            true
+        }
 
 
         val navHostFragment =
@@ -37,4 +65,13 @@ class MainActivity : AppCompatActivity() {
         else{
             navController.navigate(R.id.onBoardFragment)
         }
-    }}
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
